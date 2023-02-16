d60692c16e2f7eb49dc60101cb444597cf3c2117
lucasbcap <beal.lucas2002@gmail.com>
2023-02-16 11:13:59 +0100

    modification

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
 <?php
+require_once 'vendor/autoload.php';
+
+use custumbox\db\ConnectionFactory;
+use custumbox\dispatcher\Dispatcher;
+
+
+session_start();
+
+ConnectionFactory::setConfig("config.ini");
+
+$html = new Dispatcher();
+//$html->run();
---
+++ src/classes/auth/Auth.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace custumbox\auth;
+
+class Auth
+{
+
+}
